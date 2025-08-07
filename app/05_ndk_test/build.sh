fc234c288a54b3d4d015654a4f437c4cee5049a6
zhaoc <598184817@qq.com>
2025-08-08 00:38:20 +0800

    [问题原因]:增加说明 [修改描述]:

--- app/05_ndk_test/build.sh
+++ app/05_ndk_test/build.sh
@@ -1,6 +1,7 @@
 #/bin/bash
-
-export ANDROID_NDK=/root/libs/NDK/android-ndk-r26d
+# 说明：设置 Android NDK 路径，下载完直接解压到根目录即可。
+# https://developer.android.google.cn/ndk/downloads?hl=zh-cn
+export ANDROID_NDK=/home/zhaoc/Downloads/android-ndk-r28c
 
 rm -r build
 mkdir build && cd build
@@ -10,14 +11,14 @@ mkdir build && cd build
 # # 设置工具链
 # set(CMAKE_TOOLCHAIN_FILE ${ANDROID_NDK}/build/cmake/android.toolchain.cmake)
 # # 目标ABI
-# set(ANDROID_ABI armeabi-v7a)
+# set(ANDROID_ABI armeabi-v7a) #arm64-v8a
 # # 最低Android平台
 # set(ANDROID_PLATFORM android-22)
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
-	-DANDROID_ABI="armeabi-v7a" \
+	-DANDROID_ABI="arm64-v8a" \
 	-DANDROID_NDK=$ANDROID_NDK \
-	-DANDROID_PLATFORM=android-22 \
+	-DANDROID_PLATFORM=android-32 \
 	-DANDROID_STL=c++_shared \
 	..
 
